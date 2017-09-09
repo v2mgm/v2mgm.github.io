6f8fecdf535982ccabcdb50cf9c47a81e4c47415
v2mgm <v2mgm@yahoo.com>
2017-09-10 00:55:33 +0430

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <link href="style.css" type="text/css" rel="stylesheet"/>
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <? require_once "strings.php"  ?>
+    <title></title>
+</head>
+<body>
+
+<div id="header">
+       <div id="header-first-links">
+           <div class="left space-left-40">
+               <ul>
+                   <li><a  class="fa fa-facebook-square" href="#"></a></li>
+                   <li><a  class="fa fa-twitter-square" href="#"></a></li>
+                   <li><a class="fa fa-instagram" href="#"></a></li>
+               </ul>
+           </div>
+           <div class="right">
+               <ul>
+                   <li class="space-left-20"><a href="#"><?=HOME?></a></li>
+                   <li class="space-left-20"><a href="#"><?=ABOUT?></a></li>
+                   <li class="space-left-20"><a href="#"><?=CONTACT?></a></li>
+               </ul>
+           </div>
+       </div>
+       <div>
+           <a id="header-title" href="index.html">
+               <?=BLOG_NAME?>
+           </a>
+       </div>
+       <div id="header-second-links">
+           <ul>
+               <li class="space-left-20"><a href="#"><?=SAMPLES?></a></li>
+               <li class="space-left-20"><a href="#"><?=ANDROID_APPS?></a></li>
+               <li class="space-left-20"><a href="#"><?=OTHER_WORKS?></a></li>
+           </ul>
+       </div>
+</div>
+
+<!--<div id="content">-->
+<!--    <div class="container">-->
+<!---->
+<!--    </div>-->
+<!--</div>-->
+<!---->
+<!---->
+<!--<div id="footer">-->
+<!--    <div class="container">-->
+<!--        <ul id="header-nav">-->
+<!--            <li><a href="about.php">--><?//=CONTACT?><!--</a></li>-->
+<!--            <li><a href="about.php">--><?//=ABOUT?><!--</a></li>-->
+<!--        </ul>-->
+<!--        <span class="copyright">--><?//=COPYRIGHTS?><!--</span>-->
+<!--    </div>-->
+<!--</div>-->
+
+
+</body>
+</html>
---
+++ strings.php
@@ -0,0 +1,10 @@
+<?php
+define("HOME","صفحه اصلی");
+define("BLOG_NAME","اینجا دور است");
+define("CONTACT","تماس با ما");
+define("ABOUT","درباره ما");
+define("COPYRIGHTS","کپی رایت");
+define("SAMPLES","نمونه کارهای گیف");
+define("ANDROID_APPS","برنامه های اندرویدی من");
+define("OTHER_WORKS","دیگر درخواست ها");
+?>
